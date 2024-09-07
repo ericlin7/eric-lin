7baaa159a80b2acbc0c73ea5614c9776017341a8
ericlin7 <ericlin20002@gmail.com>
2024-09-07 15:50:10 -0700

    fix-build-issues

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -1,5 +1,11 @@
 import Home from "@/components/home";
 
+// Define the dynamic paths for static export
+export async function generateStaticParams() {
+  return [{ slug: ["path1"] }, { slug: ["path2", "subpath"] }];
+}
+
+// Page component
 function Page() {
   return <Home />;
 }
